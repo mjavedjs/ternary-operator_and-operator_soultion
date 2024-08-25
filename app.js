7d978c8d79074f7da34b623076cfb677ae500497
muhammadjaved238 <mjavedjs2006@gmail.com>
2024-08-26 02:52:01 +0500

    Ternary_operator

---
+++ app.js
@@ -0,0 +1,11 @@
+let score=+prompt("enter your age");
+let result1= score<=18 ? 'You are not eligible for driving':'You are eligible for driving';
+console.log(result1)
+// Example No 2 of ternary opertor
+let marks=+prompt("enter your marks");
+let result= marks >=60 ? 'PASSED':'FAILE';
+console.log(result);
+//Practise question
+console.log("5"-3);
+console.log(2<12<5); // 2 <12 == ture =1 and also 1<5 the answer is true
+console.log( typeof("20"+10 +10)); // 20 is string and this question concatination string plus with number and 
